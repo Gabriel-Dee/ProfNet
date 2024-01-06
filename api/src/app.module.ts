9ec162e8b120d4a4f93549fb692bb235a8be671a
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2024-01-06 15:55:58 +0300

    module imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: true,
+      // entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true, // this should not be used for production because it might lead to data loss
+      // logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
