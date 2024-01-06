1254628d57cb5f3daf3e042f5d0e557e499858c8
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2024-01-06 19:32:45 +0300

    create, post, update

--- api/src/feed/controllers/feed.controller.ts
+++ api/src/feed/controllers/feed.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { FeedService } from '../services/feed.service';
+import { FeedPost } from '../models/post.interface';
+import { Observable } from 'rxjs';
+import { UpdateResult } from 'typeorm';
 
 @Controller('feed')
-export class FeedController {}
+export class FeedController {
+  constructor(private feedService: FeedService) {}
+
+  // post request
+  @Post()
+  create(@Body() feedpost: FeedPost): Observable<FeedPost> {
+    return this.feedService.createPost(feedpost);
+  }
+  // Get request
+  @Get()
+  findAll(): Observable<FeedPost[]> {
+    return this.feedService.findAllPosts();
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body()
+    feedpost: FeedPost,
+  ): Observable<UpdateResult> {
+    return this.feedService.updatePost(id, feedpost);
+  }
+}
